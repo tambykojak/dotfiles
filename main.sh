73ceb79a91b1ba77f5bacd2ec0d81a5ffdfa6560
Tamby Kojak <tambykojak@gmail.com>
2019-01-19 17:59:24 -0500

    Added bash-completion

--- main.sh
+++ main.sh
@@ -1,5 +1,11 @@
 export PS1="$ ";
 
+if [[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]]; then
+  source "/usr/local/etc/profile.d/bash_completion.sh"
+else
+  echo "Make sure to run brew install bash-completion"
+fi
+
 google() {
     search=""
     for term in $@; do
